8db1cd40c9fc44d104e98a600dc76b70f160a5c0
Michael Johnson <mjohnson@8thlight.com>
2023-03-17 10:22:58 -0500

    Add environment variable to template and docker for testing

--- NationalGlass/Pages/Index.cshtml.cs
+++ NationalGlass/Pages/Index.cshtml.cs
@@ -6,10 +6,14 @@ namespace NationalGlass.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public readonly string Greeting;
+    public readonly string Subject;
+    
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
+        Greeting = configuration.GetSection("NG_GREETING").Value ?? "Hello";
+        Subject = configuration.GetSection("NG_SUBJECT").Value ?? "World";
     }
 
     public void OnGet()
